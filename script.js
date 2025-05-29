42d707c9c73e1fd63d9bbea6cb682cf7bcae5f87
Biozidtonoy <150583477+Biozidtonoy@users.noreply.github.com>
2025-05-29 19:12:06 +0600

    all the aretchmetic function are worked but division part have to solve

--- script.js
+++ script.js
@@ -1,38 +1,121 @@
-// addition
 function add(num1,num2){
-    return (`addition : ${num1+num2}`);
+    return Number(num1) + Number(num2);
 }
-//subtraction
+
 function sub(num1,num2){
-    return (`substruction : ${num1-num2}`);
+    return Number(num1) - Number(num2);
 }
-// multiplication
+
 function multi(num1,num2){
-    return (`multiplication: ${num1*num2}`);
+    return Number(num1) * Number(num2);
 }
-// division
+
 function divi(num1,num2){
-    return (`division : ${num1/num2}`);
+    return Number(num1) / Number(num2);
 }
 
-
-// operate the upper function
 function operate(){
-    // let firstNum = parseFloat(prompt("1st num"));
-    // let op = prompt("enter operator : +, - , * ,/");
-    // let secNum = parseFloat(prompt("2nd num"));
+    let firstNum = "";
+    let secNum = "";
+    let op = "";
+    const input = document.querySelector(".inputDisplay");
+    const output = document.querySelector(".outputDisplay");
+    const numBtns = document.querySelectorAll(".btn");
+    const clearBtn = document.querySelector(".clearBtn");
+    const opButtons = document.querySelectorAll(".btns");
+    const resBtn = document.querySelector(".resBtn");
 
-    if (op ==="+"){
-        return add(firstNum,secNum);
-    }
-    else if(op === "-"){
-        return sub(firstNum,secNum);
-    }
-    else if(op === "*"){
-        return multi(firstNum,secNum);
-    }
-    else if(op === "/"){
-        return divi(firstNum,secNum);
-    }
+    clearBtn.addEventListener('click',()=>{ 
+        firstNum = "";
+        secNum ="";
+        op = "";
+        input.textContent = "";
+        output.textContent = "";
+    })
+
+    numBtns.forEach(button =>{
+        button.addEventListener('click',() =>{
+            if(op===""){
+                firstNum += button.textContent;
+                console.log(firstNum);
+                input.textContent = `${firstNum}`;
+            }else{
+                secNum += button.textContent;
+                console.log(secNum);
+                input.textContent = `${secNum}`;
+            }
+        })
+    })
+
+    
+    opButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            if(secNum!==""){
+                if (op ==="+"){
+                    let res = add(firstNum,secNum);
+                    secNum = "";
+                    firstNum = res ;
+                    console.log(`first num : ${firstNum}`);
+                    output.textContent = `${res}`
+                }
+                else if(op === "-"){
+                    let res = sub(firstNum,secNum);
+                    secNum = "";
+                    firstNum = res ;
+                    console.log(firstNum);
+                    output.textContent = `${res}`
+                }
+                else if(op === "*"){
+                    let res = multi(firstNum,secNum);
+                    secNum = "";
+                    firstNum = res ;
+                    console.log(firstNum);
+                    output.textContent = `${res}`
+                }
+                else if(op === "/"){
+                    let res = divi(firstNum,secNum);
+                    secNum = "";
+                    firstNum = res ;
+                    console.log(firstNum);
+                    output.textContent = `${res}`
+                }
+            }
+            op = button.textContent;
+            console.log(op);
+            input.textContent = `${op}`
+        });
+    });
+    
+    resBtn.addEventListener('click',()=>{
+        if (op ==="+"){
+            let res = add(firstNum,secNum);
+            secNum = "";
+            firstNum = res ;
+            console.log(`first num : ${firstNum}`);
+            output.textContent = `${res}`
+        }
+        else if(op === "-"){
+            let res = sub(firstNum,secNum);
+            secNum = "";
+            firstNum = res ;
+            console.log(firstNum);
+            output.textContent = `${res}`
+        }
+        else if(op === "*"){
+            let res = multi(firstNum,secNum);
+            secNum = "";
+            firstNum = res ;
+            console.log(firstNum);
+            output.textContent = `${res}`
+        }
+        else if(op === "/"){
+            let res = divi(firstNum,secNum);
+            secNum = "";
+            firstNum = res ;
+            console.log(firstNum);
+            output.textContent = `${res}`
+        }
+    })
+   
 }
 console.log(operate());
